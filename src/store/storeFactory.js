2c82f8c92f51165c607a8d4b887e1ebb773abdff
Joseph Schoech <js@js-cpu.local>
2017-12-13 17:41:52 -0700

    stylin

--- src/store/storeFactory.js
+++ src/store/storeFactory.js
@@ -1,11 +1,11 @@
 import { createStore, combineReducers } from 'redux'
 import { posts, user } from './reducers'
 import stateDate from '../data/initialState'
-import { composeWithDevTools } from 'redux-devtools-extension'
+//import { composeWithDevTools } from 'redux-devtools-extension'
 
 
 const storeFactory = (initialState=stateDate) => 
-	createStore(combineReducers({posts, user}), initialState, composeWithDevTools())
+	createStore(combineReducers({posts, user}), initialState /*, composeWithDevTools()*/)
 
 
 export default storeFactory
