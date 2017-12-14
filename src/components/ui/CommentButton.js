335731cb725bd9d185d1236e8949184791c2210d
Joseph Schoech <js@js-cpu.local>
2017-12-13 18:50:16 -0700

    add comment button

---
+++ src/components/ui/CommentButton.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CommentButton = () => {
+
+	return (
+	      <svg version="1.1" className='height-ml' id="Layer_1" x="0px" y="0px"
+	     viewBox="0 0 612 612">
+			<g>
+			    <path className='comment-svg' fill='#ff0000' d="M306,0C137,0,0,119.9,0,267.8c0,84.5,44.8,159.8,114.8,208.8V612l134-81.3c18.6,3.1,37.6,4.8,57.2,4.8
+				c169,0,306-119.9,306-267.8S475,0,306,0z"/>
+			</g>
+		</svg>
+	)
+}
+export default CommentButton
--- src/components/ui/FeedNode.js
+++ src/components/ui/FeedNode.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import FavButton from './FavButton'
 import NahButton from './NahButton'
+import CommentButton from './CommentButton'
 import AvatarImage  from './AvatarImage'
 import { Link } from 'react-router-dom'
 import { submitFav, submitNah } from '../../lib/functions'
@@ -21,7 +22,7 @@ const FeedNode = ({id='', user='', avatar='', text='', rating=0, link='', timest
                 <div className='margin-t-s font-sm flex-container'>
                     <div className='height-ml block nah-none'><AvatarImage avatarName={avatar} /></div>
                     <div className='margin-sides-xxs flex-center flex-grow nah-none'><a href={link}>{user}</a></div>
-                    <div className='flex-center flex-right margin-sides-m cursor-pointer'><Link className='nested-diplpay-n nah-none' to={id}>Comment</Link></div>
+                    <div className='flex-center flex-right margin-sides-m cursor-pointer nah-none'><Link className='nested-diplpay-n nah-none' to={id}><CommentButton /></Link></div>
                     <div className='flex-center flex-right margin-sides-m cursor-pointer nah-none' onClick={submitF}><FavButton /></div>
                     <div className='flex-center cursor-pointer flex-right' onClick={submitN}><NahButton /></div>
                 </div>
